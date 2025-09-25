1c2dccab15e8a95273f55d8d2aa4b422d6b43da2
ilukssa <ilya.sakantsev@mail.ru>
2025-09-25 23:52:33 +0300

    add Images extensions

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import os
 # Создавать всю структуру папок сразу же? Или создавать по мере появления новых файлов?
 
 EXTENSIONS = {
-    "Text":[".txt", ".log", ".md", ".csv", ".tsv", ".json", ".xml", ".html",]}
+    "Text":[
+        ".txt", ".log", ".md", ".csv", ".tsv", ".json", ".xml", ".html",
+    ],
+    "Images":[
+        ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".heic", ".heif",
+        ".raw", ".cr2", ".nef", ".arw", ".orf", ".rw2", ".svg",
+    ],
+
+}
 
 def main():
     pass
