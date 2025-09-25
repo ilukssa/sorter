7b92c92889d219797770547761d530a52be2d6cf
ilukssa <ilya.sakantsev@mail.ru>
2025-09-26 00:01:56 +0300

    ref extensions

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import os
 # Создавать всю структуру папок сразу же? Или создавать по мере появления новых файлов?
 
 EXTENSIONS = {
-    "Text":[
-        ".txt", ".log", ".md", ".csv", ".tsv", ".json", ".xml", ".html",
-    ],
     "Images":[
         ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".heic", ".heif",
         ".raw", ".cr2", ".nef", ".arw", ".orf", ".rw2", ".svg",
     ],
-
+    "Videos":[
+        ".mp4", ".mkv", ".avi", ".mov", ".wmv",
+        ".flv", ".webm", ".m4v", ".mpg", ".mpeg", ".3gp",
+    ],
+    "Documents":[
+        ".pdf", ".doc", ".docx", ".xls", ".xlsx",
+        ".ppt", ".pptx", ".odt", ".ods", ".odp",
+        ".rtf", ".txt", ".csv", ".md", ".epub", ".tex"
+    ],
 }
 
 def main():
